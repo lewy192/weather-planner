96ab6564c01d40bd6645bc62431be4ce08663d7d
lewy192 <lewis.james.hill@outlook.com>
2021-04-14 19:08:07 +1000

    structure of api call set out

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,11 @@
 const cardContainer = document.querySelector(".card-forcast-container");
 const citySelection = document.querySelector(".city-selection-container");
 const currentCityForcast = document.querySelector(".current-city-weather");
+const citySearchButton = document.querySelector(".city-search-button");
 
 const apiKey = "7e5922ef7f6bc85e485e53b28667f43a";
+
+const urlWeatherByCityName = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
 function displayCurrentWeather(currentData) {
     const tempFigure = document.querySelector(".temp-figure");
@@ -26,4 +29,13 @@ function getCurrentWeather(apiKey) {
         });
 }
 
+function executeCityWeatherSearch() {
+    const cityNameInput = document.querySelector(".city-search-input");
+}
+
 getCurrentWeather(apiKey);
+
+citySearchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    executeCityWeatherSearch();
+});
