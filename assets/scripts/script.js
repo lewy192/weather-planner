8504efde1a18d81f7d85feb130805892addad3c6
lewy192 <lewis.james.hill@outlook.com>
2021-04-23 16:37:32 +1000

    fully functional weather dashboard, bug testing and error handling needed to be finished

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,6 +24,7 @@ function displayCurrentWeather(cityCoords) {
             throw new Error("Try a Different city");
         })
         .then((data) => {
+            // TODO: delete errorPara and add city name header back
             cityNameHeader.textContent = data.timezone.split("/")[1];
             document
                 .querySelectorAll(".figure")
@@ -31,17 +32,23 @@ function displayCurrentWeather(cityCoords) {
             windSpeedFigure.textContent = windSpeedFigure.textContent = `Wind Speed:${data.current.wind_speed}`;
             tempFigure.textContent = `Temperature:${data.current.temp}`;
             humidFigure.textContent = `Humidity:${data.current.humidity}%`;
-            uvIndexFigure.textContent = `UV-Index:${data.current.uvi}`;
+            uvIndexFigure.textContent = `UV-Index:`;
+            const uvIndexNumber = document.createElement("span");
+            uvIndexNumber.textContent = `${data.current.uvi}`;
+            uvIndexColour(data.current.uvi, uvIndexNumber);
+            uvIndexFigure.appendChild(uvIndexNumber);
+            fiveDayForecastCards(data);
         })
-        .catch((err) => cityName.textContent);
+        .catch((err) => {
+            cityName.textContent;
+        });
 }
 
 function getCityCoords(apiKey, cityName) {
-    // const urlWeatherByCityName = `http://api.openweathermap.org/geo/1.0/direct?q=melbourn,au&appid=${apiKey}`;
+    // const urlWeatherByCityName = `http://api.openweathermap.org/geo/1.0/direct?q=melbourne,au&appid=${apiKey}`;
     const urlWeatherByCityName = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
     fetch(urlWeatherByCityName)
         .then(function (response) {
-            console.log(response);
             if (response.ok) return response.json();
             throw new Error("Geocoding Api Failed");
         })
@@ -56,19 +63,18 @@ function getCityCoords(apiKey, cityName) {
                     "ERROR:\nTry another city. \nMake sure you enter it in the format:\nCityName,CountryCode";
                 errorParagraph.style.fontSize = "16px";
                 errorParagraph.style.fontWeight = "bold";
+                // TODO: add class to errorPara
                 currentCityForcast.appendChild(errorParagraph);
             } else {
                 cityNameHeader.textContent =
                     "Geoencoding Api failed, try again later.";
             }
-            // console.log(err);
             // Notify user they have requested a city thats not availible
         });
 }
 
 function executeCityWeatherSearch() {
     const cityNameInput = document.querySelector(".city-search-input");
-    console.log(cityNameInput.value);
     getCityCoords(apiKey, cityNameInput.value);
 }
 
@@ -77,4 +83,51 @@ function executeCityWeatherSearch() {
 citySearchButton.addEventListener("click", function (event) {
     event.preventDefault();
     executeCityWeatherSearch();
+});
+
+function uvIndexColour(uvindex, element) {
+    if (uvindex < 3) {
+        element.classList.add("uv-low");
+    } else if (uvindex < 8 && !uvindex < 3) {
+        element.classList.add("uv-mid");
+    } else {
+        element.classList.add("uv-high");
+    }
+    element.classList.add("uv-index-fig");
+}
+
+function fiveDayForecastCards(data) {
+    cardContainer.querySelectorAll("*").forEach((child) => child.remove());
+    const fiveDayForecastData = [];
+    for (let i = 1; i < 6; i++) {
+        fiveDayForecastData.push(data.daily[i]);
+    }
+    fiveDayForecastData.forEach((day) => {
+        const dayCard = document.createElement("div");
+        dayCard.classList.add("card");
+        dayCard.classList.add("forecast-card");
+
+        const dateHeader = document.createElement("h2");
+        dateHeader.textContent = new Date(day.dt * 1000).toLocaleDateString(
+            "en-US"
+        );
+        const icon = document.createElement("img");
+        icon.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+        const temperatureParagraph = document.createElement("p");
+        temperatureParagraph.textContent = `Temperature:${day.temp.day}`;
+        const humidParagraph = document.createElement("p");
+        humidParagraph.textContent = `Humidity:${day.humidity}`;
+        dayCard.appendChild(dateHeader);
+        dayCard.appendChild(icon);
+        dayCard.appendChild(temperatureParagraph);
+        dayCard.appendChild(humidParagraph);
+        cardContainer.appendChild(dayCard);
+    });
+}
+
+citySelection.addEventListener("click", (event) => {
+    if (event.target.matches(".city-button")) {
+        // (event.target.textContent);
+        getCityCoords(apiKey, event.target.textContent);
+    }
 });
